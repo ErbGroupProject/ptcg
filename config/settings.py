e0e0c597982d2a836276dd424030f333b9b26a9e
= <johnnyleecheukyan@gmail.com>
2026-08-07 16:14:59 +0800

    added app pages and home and shops

--- config/settings.py
+++ config/settings.py
@@ -45,8 +45,8 @@ DJANGO_APPS = [
 ]
 
 LOCAL_APPS = [
-    "filters.apps.FiltersConfig",
-
+    # 'filters.apps.FiltersConfig'
+    "pages.apps.PagesConfig",
 ]
 
 
@@ -67,16 +67,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
