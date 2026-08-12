8a4419c2706c342a5ef9a8d3c76f37911b8219ad
RavioEnby <jackhui328@gmail.com>
2026-08-12 12:32:33 +0800

    premarge

--- config/settings.py
+++ config/settings.py
@@ -92,11 +92,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ptcg_db",
+        "USER": "ptcg_user",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
