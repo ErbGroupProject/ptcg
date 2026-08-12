742c1ea98eba37ac164556f0c2070eb11b99eb2e
orik-L <orik6891@gmail.com>
2026-08-12 15:38:30 +0800

    edited base.html,_navbar,_footer,index

--- config/urls.py
+++ config/urls.py
@@ -20,13 +20,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
-from pages.views import index
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('', index, name='home'),
+    path('', include("pages.urls",namespace='pages')),
+    path('admin/', admin.site.urls)
+    # path('filters/', include("filters.urls")),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+debug_toolbar_urls()
 
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
