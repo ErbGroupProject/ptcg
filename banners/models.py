960893d0bde8291acb327710c6949456b73bf7f1
Tonydetiger <tony_chiu_2002@hotmail.com>
2026-08-13 14:14:00 +0800

    20260813updated_v2

--- banners/models.py
+++ banners/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Banner(models.Model):
+    banner_image=models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    link=models.CharField(max_length=200)
