a45f6356e4e81695a1a9571ef9af4bb9dab0b92f
= <johnnyleecheukyan@gmail.com>
2026-08-13 14:56:18 +0800

    accounts+register+login+shops

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
 from . import views
 
-<<<<<<< HEAD
 app_name = "accounts"
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-=======
-app_name="accounts"
-urlpatterns=[
-    path('register/',views.register,name='register'),
-    path('login/',views.login,name='login'),
->>>>>>> origin/ki2
 ]
--- config/urls.py
+++ config/urls.py
@@ -22,17 +22,11 @@ from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('', index, name='home'),
-=======
     path('', include("pages.urls",namespace='pages')),
     path('shops/', include("shops.urls",namespace='shops')),
     path('accounts/', include("accounts.urls",namespace='accounts')),
     path('admin/', admin.site.urls)
     # path('filters/', include("filters.urls")),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+debug_toolbar_urls()
->>>>>>> origin/ki2
 
 
