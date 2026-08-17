1105ab98b4cdca064f51ae45c1c8ac9a82c00b10
= <johnnyleecheukyan@gmail.com>
2026-08-17 11:42:01 +0800

    bug fix

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-<<<<<<< HEAD
 from django.contrib.auth.models import User
-=======
 from django.db.models.signals import post_save
 from django.dispatch import receiver
->>>>>>> johnny
 
 
 class Profile(models.Model):
