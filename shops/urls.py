b7b862b9127dc6ee5c797cde0428d158e537169c
orik-L <orik6891@gmail.com>
2026-08-18 12:30:09 +0800

    updated:admin pages-aug-18, 12:29

--- shops/urls.py
+++ shops/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name="shops"
 urlpatterns=[
-    path('',views.shop,name='shop'),
+    path('shop<int:listing_id',views.shop,name='shop'),
     path('search_shops/',views.search_shops,name='search_shops'),
 ]
