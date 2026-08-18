c1119812369f0b1107ad9818ec76d24a079e02cb
orik-L <orik6891@gmail.com>
2026-08-18 11:46:29 +0800

    merged aug18:11:47

--- shops/admin.py
+++ shops/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Shoplist
 
-# Register your models here.
+class ShoplistAdmin(admin.ModelAdmin):
+    list_display = ('shopname','address')
+    list_display_links =('shopname','address')
+    search_fields = 'shopname','address',
+    list_per_page = 25
+
+admin.site.register(Shoplist,ShoplistAdmin)
