efac4586d70a0ef4e55cca7e7a9870d67c6b3599
orik-L <orik6891@gmail.com>
2026-08-20 13:18:17 +0800

    shop app done

--- shops/models.py
+++ shops/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .choices import district_choices
+from geopy.geocoders import ArcGIS
 
 class Shoplist(models.Model):
     shopname = models.CharField(max_length=200)
@@ -16,6 +17,23 @@ class Shoplist(models.Model):
     card_identification = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=20, blank=True)
     shop_logo = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True,null=True)
+    latitude = models.FloatField(null=True, blank=True, verbose_name='緯度')
+    longitude = models.FloatField(null=True, blank=True, verbose_name='經度')
+
+    def save(self, *args, **kwargs):
+        if self.address:
+            try:
+                geolocator = ArcGIS()
+                location = geolocator.geocode(query=self.address)
+                if location:
+                    self.latitude = location.latitude
+                    self.longitude = location.longitude
+                    print(f"✅ 地圖自動匹配成功！座標為: {location.latitude}, {location.longitude}")
+                else:
+                    print("⚠️ 警告：Google/OSM 找不到這個地址，請嘗試精簡地址字串！")
+            except Exception as e:
+                print(f"❌ 地圖自動匹配失敗！錯誤原因為: {e}")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.shopname
